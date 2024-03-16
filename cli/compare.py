5ac07b80f997386c1203b4154a5d99c19c358d89
mmmmaxonchik <maxspytcin@gmail.com>
2024-03-16 22:03:40 +0300

    init

---
+++ cli/compare.py
@@ -0,0 +1,67 @@
+import asyncio
+import csv
+import os
+import numpy as np
+import xgboost
+
+from web3 import AsyncWeb3
+from report import Wallet
+from etherscan_sdk.sdk import Account
+from compare_clis.cryptowallet_risk_scoring.januus_riskreport.client import riskreport_on_entity
+import pandas as pd
+
+
+async def main():
+    ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
+    if ETHERSCAN_API_KEY is None:
+        print("Необходим etherscan api key для получения транзакций!")
+        os._exit(1)
+
+    INFURA_API_KEY = os.getenv("INFURA_API_KEY")
+    if INFURA_API_KEY is None:
+        print("Необходим infura api key для запросов в сеть Ethereum!")
+        os._exit(1)
+
+    wallets = pd.read_csv("./data/wallets.csv")
+
+    model = xgboost.XGBClassifier()
+    model.load_model("./model.json")
+
+    it = 0
+    with open("./data/compare_history.csv", "a", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+
+        for row in list(wallets.iterrows())[4285:]:
+            if it % 10 == 0:
+                file.flush()
+
+            row = row[1]
+            address = row["Address"]
+            is_real_fraud = row["Fraud"]
+
+            account = Account(ETHERSCAN_API_KEY, address)
+            provider = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(
+                f"https://mainnet.infura.io/v3/{INFURA_API_KEY}")
+            )
+
+            wallet = Wallet(account, provider)
+
+            report = await wallet.get_report()
+            report_array = np.array([report])
+
+            prediction_own = model.predict(report_array)[0]
+
+            prediction_compare = 0 if riskreport_on_entity(
+                eth_addresses=[address]).risk_scores.fraud_risk < 60 else 1
+
+            writer.writerow(
+                [
+                    address, is_real_fraud, prediction_own, prediction_compare
+                ]
+            )
+            it += 1
+            print(it)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- cli/main.py
+++ cli/main.py
@@ -1,25 +1,35 @@
 import asyncio
+import csv
 import os
+import numpy as np
+import xgboost
 
 from web3 import AsyncWeb3
-import xgboost
 from report import Wallet
 from etherscan_sdk.sdk import Account
-import numpy as np
+from compare_clis.cryptowallet_risk_scoring.januus_riskreport.client import riskreport_on_entity
 
 
 async def main():
     ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
     if ETHERSCAN_API_KEY is None:
-        print("")
+        print("Необходим etherscan api key для получения транзакций!")
         os._exit(1)
 
     INFURA_API_KEY = os.getenv("INFURA_API_KEY")
     if INFURA_API_KEY is None:
-        print("")
+        print("Необходим infura api key для запросов в сеть Ethereum!")
         os._exit(1)
 
     address = input("Введите проверяемый адрес: ")
+
+    is_real_fraud = int(input(
+        "Является ли проверяемый адрес мошенническим:\n0) Нет\n1) Да\n2) Не знаю\n"
+    ))
+
+    if is_real_fraud not in range(0, 3):
+        print("Введено не верное значение!")
+        os._exit(1)
 
     account = Account(ETHERSCAN_API_KEY, address)
     provider = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(
@@ -28,22 +38,22 @@ async def main():
 
     wallet = Wallet(account, provider)
 
-    report = await wallet.get_report()
-    report_array = np.array([report])
-
     model = xgboost.XGBClassifier()
     model.load_model("./model.json")
 
-    prediction = model.predict(report_array)
+    report = await wallet.get_report()
+    report_array = np.array([report])
+
+    prediction = model.predict(report_array)[0]
 
     if prediction == 1:
         print(f"Адрес: {address} является мошенническим")
     else:
         print(f"Address: {address} является нормальным")
 
+    with open("./data/history.csv", "a", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([address, is_real_fraud, prediction])
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-# "0xD816FB91C7688eCB56A75757AF3631821297bC4b" - Fraud - 1
-# "0x2e16bd350cb5db830fb50328ab4df62fd4c16ae8" - Fraud - 1
